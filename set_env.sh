cc97627520ff8fc5e771154af5b5ea064f5b416b
Sage <tuzzy08@gmail.com>
2022-10-26 03:23:19 +0100

    initial commit

--- set_env.sh
+++ set_env.sh
@@ -1,10 +1,10 @@
 # This file is used for convenience of local development.
 # DO NOT STORE YOUR CREDENTIALS INTO GIT
 export POSTGRES_USERNAME=postgres
-export POSTGRES_PASSWORD=mypassword
-export POSTGRES_HOST=postgres.cr9bldgsf1j6.us-east-1.rds.amazonaws.com
-export POSTGRES_DB=postgres
-export AWS_BUCKET=arn:aws:s3:::mybucket1200798
+export POSTGRES_PASSWORD=Inspiron08.
+export POSTGRES_HOST=monolith-sage-dev.cfkpz72o8fn7.us-east-1.rds.amazonaws.com
+export POSTGRES_DB=monolith_sage_dev
+export AWS_BUCKET=arn:aws:s3:::microservices-sage-dev
 export AWS_REGION=us-east-1
 export AWS_PROFILE=default
 export JWT_SECRET=testing
